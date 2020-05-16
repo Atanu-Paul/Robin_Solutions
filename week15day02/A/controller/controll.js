0b3a788638a06efe5c8d87c0d5002e419b242d2e
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-05-17 01:32:50 +0530

    week15 day02 codes updated

--- week15day02/A/controller/controll.js
+++ week15day02/A/controller/controll.js
@@ -12,4 +12,67 @@ controll.displayAllData = (req, res) => {
   res.status(200).render("page2", { title: "All Data", data: data });
 };
 
+controll.displayOne = (req, res) => {
+  var query = req.query.id;
+
+  for (i = 0; i <= data.length; i++) {
+    if (query == i) {
+      var query_result = data[i - 1];
+    }
+  }
+  res.status(200).render("page3", { tile: "One Data", data: query_result });
+};
+
+controll.addDataForm = (req, res) => {
+  res.status(200).render("page4", { title: "Add Data" });
+};
+
+controll.addData = (req, res) => {
+  const newObj = ({ id, userId, title, body } = req.body);
+  console.log(newObj);
+  data.push(newObj);
+  res.status(200).redirect("show");
+
+  // const newObj = { ...req.body };
+  // data.push(newObj);
+  // res.status(200).json({ status: "Data Added", data: data });
+};
+
+controll.updateDataForm = (req, res) => {
+  res.status(200).render("page5", { title: "Update Data" });
+};
+
+controll.updateData = (req, res) => {
+  var query = req.body.id;
+  let obj = getItem(query);
+  const body = { ...req.body };
+  let id = obj.element.id;
+  body.id = id;
+
+  data.splice(obj.index, 1, body);
+  res.status(200).redirect("show");
+};
+
+controll.deleteDataForm = (req, res) => {
+  res.status(200).render("page6", { title: "Delete Data" });
+};
+
+controll.deleteData = (req, res) => {
+  var query = req.body.id;
+  let obj = getItem(query);
+  let item = data.splice(obj.index, 1)[0];
+
+  res.status(200).redirect("show");
+};
+
+function getItem(id) {
+  let item1 = null;
+  data.forEach((element, index) => {
+    if (String(element.id) === id) {
+      item1 = { element, index };
+    }
+  });
+  return item1;
+}
+
 module.exports = controll;
--- week15day02/A/route/route.js
+++ week15day02/A/route/route.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const route = express.Router();
 
-var { defaultroute, displayAllData } = require("../controller/controll");
+var {
+  defaultroute,
+  displayAllData,
+  displayOne,
+  addDataForm,
+  addData,
+  updateDataForm,
+  updateData,
+  deleteDataForm,
+  deleteData,
+} = require("../controller/controll");
 
 route.get("/", defaultroute);
 route.get("/show", displayAllData);
+route.get("/show/id", displayOne);
+route.get("/add", addDataForm);
+route.get("/update", updateDataForm);
+route.get("/delete", deleteDataForm);
+route.post("/addData", addData);
+route.put("/updateData", updateData); //use this route on postman to see the put request in action
+route.post("/updateData", updateData); //use this route on the browser to see the same
+route.delete("/deleteData", deleteData); //use this route on postman to see the put request in action
+route.post("/deleteData", deleteData); //use this route on the browser to see the same
 
 module.exports = route;
